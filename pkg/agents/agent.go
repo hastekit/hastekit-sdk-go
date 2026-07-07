ab6c47ba78d90251fb47087d8ac1df08dc42ba62
praveen <praveenraj9495@gmail.com>
2026-07-07 10:24:52 +0530

    fix deferred tool description limit

--- pkg/agents/agent.go
+++ pkg/agents/agent.go
@@ -399,7 +399,7 @@ func (e *Agent) ExecuteWithRun(ctx context.Context, in *AgentInput, run *history
 				}
 				info := DeferredToolInfo{Name: schema.OfFunction.Name}
 				if schema.OfFunction.Description != nil {
-					info.Description = strings.SplitN(*schema.OfFunction.Description, "\n", 1)[0]
+					info.Description = strings.SplitN(strings.TrimPrefix(*schema.OfFunction.Description, "\n"), "\n", 2)[0]
 				}
 				deferredToolInfos = append(deferredToolInfos, info)
 			}
